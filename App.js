9bd2d0f64cea5daf0ee830e512508863f81e3927
Marcello Caccamo <londonliving89@gmail.com>
2022-03-14 11:08:18 +1000

    last touches

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import * as Font from "expo-font";
-import AppLoading from 'expo-app-loading';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
-import Header from "./components/Header";
-import StartGameScreen from "./screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
-import GameOverScreen from "./screens/GamoOverScreen";
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
 };
 
@@ -25,7 +25,7 @@ export default function App() {
       <AppLoading
         startAsync={fetchFonts}
         onFinish={() => setDataLoaded(true)}
-        onError={(err) => console.log(err)}
+        onError={err => console.log(err)}
       />
     );
   }
@@ -35,12 +35,11 @@ export default function App() {
     setUserNumber(null);
   };
 
-  const startGameHandler = (selectedNumber) => {
+  const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
-    setGuessRounds(0);
   };
 
-  const gameOverHandler = (numOfRounds) => {
+  const gameOverHandler = numOfRounds => {
     setGuessRounds(numOfRounds);
   };
 
@@ -70,6 +69,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
